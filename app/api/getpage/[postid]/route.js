2c50a79ced003ce92c079c327d7fc6940cf01bf2
Samir Tiwari <samirtiwari2061@gmail.com>
2024-05-13 14:02:22 +0545

    fix bug

--- app/api/getpage/[postid]/route.js
+++ app/api/getpage/[postid]/route.js
@@ -8,17 +8,24 @@ export async function GET(req, res) {
   try {
     await connectDB();
 
-    console.log("yo req ho page ko :", req);
+    // console.log("yo req ho page ko :", req);
     // console.log("yo search ho page ko :", req.search);
     // console.log("yo searchParams ho page ko :", req.searchParams);
-    console.log("yo sano url ho page ko :", req.url);
+    // console.log("yo sano url ho page ko :", req.url);
     // console.log("yo thulo URL ho page ko :", req.URL);
     const id = await req.url.split("=")[1];
-    console.log("yo pageid ho page ko :", id);
+    // console.log("yo pageid ho page ko :", id);
 
+    console.log("/*******************************************11111 ");
     const pageDetail = await donationPage.find({ _id: id });
+    console.log("yo pageDetail  ho page ko :", pageDetail);
+    console.log("/*******************************************22222 ");
     const organiser = await User.find({ _id: pageDetail.user });
+    console.log("yo organiser  ho page ko :", organiser);
+
+    console.log("/*******************************************22222 ");
     const pay = await Payment.find({ to_page: id, done: true });
+    console.log("yo pay  ho page ko :", pay);
 
     if (pageDetail) {
       return NextResponse.json({
