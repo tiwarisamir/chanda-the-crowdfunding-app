cc8479c417b29ef21a0ea221cf4f0873c992bc67
Samir Tiwari <samirtiwari2061@gmail.com>
2024-05-13 12:15:41 +0545

    fix bug

--- app/api/getpage/[postid]/route.js
+++ app/api/getpage/[postid]/route.js
@@ -8,21 +8,21 @@ export async function GET(req, res) {
   try {
     await connectDB();
 
-    const id = await req.url.split("=")[1];
-    console.log("yo id ho page ko :", req);
+    console.log("yo req ho page ko :", req);
+    // const id = await req.url.split("=")[1];
 
-    const pageDetail = await donationPage.find(id);
-    const organiser = await User.find(pageDetail.user);
-    const pay = await Payment.find({ to_page: id, done: true });
+    // const pageDetail = await donationPage.find(id);
+    // const organiser = await User.find(pageDetail.user);
+    // const pay = await Payment.find({ to_page: id, done: true });
 
-    if (pageDetail) {
-      return NextResponse.json({
-        success: true,
-        organiser: organiser,
-        pageDetails: pageDetail,
-        recentDonation: pay,
-      });
-    }
+    // if (pageDetail) {
+    //   return NextResponse.json({
+    //     success: true,
+    //     organiser: organiser,
+    //     pageDetails: pageDetail,
+    //     recentDonation: pay,
+    //   });
+    // }
 
     return NextResponse.json({
       success: false,
