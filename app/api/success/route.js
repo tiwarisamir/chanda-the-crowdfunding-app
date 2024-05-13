3d40768eb63c3c4c480cb2153f84e6e1d57206b0
Samir Tiwari <samirtiwari2061@gmail.com>
2024-05-13 19:05:37 +0545

    fix bug

--- app/api/success/route.js
+++ app/api/success/route.js
@@ -4,25 +4,12 @@ import { createSignature } from "../handlepay/route";
 import Payment from "@/models/Payment";
 import connectDB from "@/db/connectDB";
 import donationPage from "@/models/donationPage";
-import { parse } from "url";
 
 export async function GET(req, res) {
   try {
-    // console.log("yo query ho in success url :", req.query);
-    // const parsedUrl = parse(req.url, true);
-    // console.log("yo req ho", req);
-    // console.log("yo req.query ho", req.query);
-    // console.log("yo req.url ho", req.url);
-    // const data = parsedUrl.query.data;
-    // console.log("yo data ho", data);
     const id = await req.url.split("=")[1];
-    // const { data } = req.query;
-
-    // const parsedUrl = parse(req.url, true);
-    // const data = parsedUrl.query.data;
 
     const decodeData = JSON.parse(Buffer.from(id, "base64").toString("utf-8"));
-    // console.log("yo decoded data ho :", decodeData);
 
     const message = decodeData.signed_field_names
       .split(",")
